769de0c0fb0de55939276ed13023b02bf4c48daa
Noa <noa.julkunen@gmail.com>
2025-09-16 15:55:45 +0300

    Uudelleenkirjoitettu ajan käsittelyä.

--- utils/timeUtils.js
+++ utils/timeUtils.js
@@ -8,11 +8,12 @@ function convertDateToIsoFormat(dateStr, timeStr) {
   month = month.padStart(2, "0");
 
   let [hour, minute] = timeStr.split(":").map(Number);
-  const date = new Date(`${year}-${month}-${day}T03:00:00`);
-  date.setHours(date.getHours() + hour);
-  date.setMinutes(date.getMinutes() + minute);
-  return date;
+
+  const utcDate = new Date(Date.UTC(year, month - 1, day, hour, minute));
+
+  return utcDate.toISOString();
 }
+
 console.log(convertDateToIsoFormat("22.09.2025", "07:07"));
 module.exports = {
   convertTimeToMinutes,
